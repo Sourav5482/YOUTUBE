9bbd6997be63374312f11d4e75b478118f6bdc72
SOURAV DAS <bcasourav5482@gmail.com>
2025-01-26 01:47:49 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+function openVideo() {
+  // Replace with your desired YouTube video URL
+  const videoURL = 'https://www.freepik.com/premium-vector/middle-finger-hand-svg-humor-funny_150936251.htm'; // Example: Rick Astley's Never Gonna Give You Up
+  window.open(videoURL, '_blank');
+}
