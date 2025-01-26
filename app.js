e7c202cb1caa162d929a905cd2a9554ded47df2c
SOURAV DAS <bcasourav5482@gmail.com>
2025-01-26 13:44:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function openVideo() {
   // Replace with your desired YouTube video URL
-  const videoURL = 'https://www.freepik.com/premium-vector/middle-finger-hand-svg-humor-funny_150936251.htm'; // Example: Rick Astley's Never Gonna Give You Up
+  const videoURL = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.npr.org%2Fsections%2F13.7%2F2015%2F09%2F23%2F442860957%2Fshould-a-monkey-own-a-copyright&psig=AOvVaw2gkMo1IzP-mv8ZNJ-Jotb7&ust=1737965634054000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJC0ndT4kosDFQAAAAAdAAAAABAE'; // Example: Rick Astley's Never Gonna Give You Up
   window.open(videoURL, '_blank');
 }
